e4f950f6a5bb3749cef4524cbd227821a9c16400
nahyarabs <nahyarabs@gmail.com>
2023-10-15 19:28:04 -0300

    atualizando controller e service

--- src/main/java/dio/design/patterns/spring/Application.java
+++ src/main/java/dio/design/patterns/spring/Application.java
@@ -2,7 +2,9 @@ package dio.design.patterns.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class Application {
 
---
+++ src/main/java/dio/design/patterns/spring/service/ViaCepService.java
@@ -0,0 +1,17 @@
+package dio.design.patterns.spring.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import dio.design.patterns.spring.entities.Endereco;
+
+    
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+	@GetMapping("/{cep}/json/")
+	Endereco consultarCep(@PathVariable("cep") String cep);
+
+    
+}
