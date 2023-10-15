275d6fbcaa41ea37b3e20312d77abbc9d98358d0
nahyarabs <nahyarabs@gmail.com>
2023-10-15 16:32:49 -0300

    inserindo pasta service

---
+++ src/main/java/dio/design/patterns/spring/service/ClienteService.java
@@ -0,0 +1,17 @@
+package dio.design.patterns.spring.service;
+
+import dio.design.patterns.spring.entities.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
