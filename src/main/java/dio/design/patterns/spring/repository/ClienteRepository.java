f7bacc0646adef45a3a618b4e93cde96b8a5de7b
nahyarabs <nahyarabs@gmail.com>
2023-10-15 19:25:51 -0300

    corrigindo parametro na classe ClienteRepository

--- src/main/java/dio/design/patterns/spring/repository/ClienteRepository.java
+++ src/main/java/dio/design/patterns/spring/repository/ClienteRepository.java
@@ -3,9 +3,9 @@ package dio.design.patterns.spring.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import dio.design.patterns.spring.entities.Endereco;
+import dio.design.patterns.spring.entities.Cliente;
 
 @Repository
-public interface ClienteRepository extends CrudRepository <Endereco, String> {
+public interface ClienteRepository extends CrudRepository <Cliente, Long> {
     
 }
